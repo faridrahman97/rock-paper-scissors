eb9e2fec1332df150421c16752c6458e5c3e6a82
Farid Rahman <beatsbyfar.id@gmail.com>
2022-08-04 10:55:54 -0600

    Fix logic for game by manipulating playerWins variable

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let tally = 0;
+    let tally = 0
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === computerSelection) { 
         console.log('Tie Game!');
@@ -13,17 +13,17 @@ function playRound(playerSelection, computerSelection) {
         console.log('You Lose! Paper beats Rock');
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         console.log('You Win! Rock beats Scissors!');
-        tally+=1
+        tally+=1;
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         console.log('You Win! Paper beats Rock');
-        tally+=1
+        tally+=1;
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
         console.log('You Lose! Scissors beats Paper!');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         console.log('You Lose! Rock beats Scissors');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         console.log('You Win! Scissors beats Paper!');
-        tally+=1
+        tally+=1;
     } else {
         console.log('Invalid input! Please put Rock, Paper, or Scissors');
     }
@@ -32,21 +32,19 @@ function playRound(playerSelection, computerSelection) {
 }
 
 let game = () => {
-    playerWins = 0
+    let playerWins= 0;
     for (let i = 0; i < 5; i++) {
         let playerChoice = prompt('Choose Rock, Paper, or Scissors');
-        playerWins = playRound(playerChoice, getComputerChoice());
+        playerWins += playRound(playerChoice, getComputerChoice());
     }
 
     if (5 - playerWins <= 2) {
         console.log("You won! You beat the computer!");
+    } else if (playerWins === 0) {
+        console.log("It was a tie game!");
     } else {
         console.log("Sorry! You lost to the computer :(");
     }
 }
-
-console.log(getComputerChoice())
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
 
 game();
