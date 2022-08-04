6c9ede04ac0154dd3ec4f5394ff58abc2cc5cdf7
Farid Rahman <beatsbyfar.id@gmail.com>
2022-08-03 22:41:47 -0600

    Add logic to play 5 rounds and then display the overall winner.

--- js/main.js
+++ js/main.js
@@ -5,30 +5,48 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let finalString = ''
+    let tally = 0;
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === computerSelection) { 
-        finalString = 'Tie Game!';
+        console.log('Tie Game!');
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        finalString = 'You Lose! Paper beats Rock';
+        console.log('You Lose! Paper beats Rock');
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        finalString = 'You Win! Rock beats Scissors!';
+        console.log('You Win! Rock beats Scissors!');
+        tally+=1
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        finalString = 'You Win! Paper beats Rock';
+        console.log('You Win! Paper beats Rock');
+        tally+=1
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        finalString = 'You Lose! Scissors beats Paper!';
+        console.log('You Lose! Scissors beats Paper!');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        finalString = 'You Lose! Rock beats Scissors';
+        console.log('You Lose! Rock beats Scissors');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        finalString = 'You Win! Scissors beats Paper!';
+        console.log('You Win! Scissors beats Paper!');
+        tally+=1
     } else {
-        finalString = 'Invalid input! Please put Rock, Paper, or Scissors';
+        console.log('Invalid input! Please put Rock, Paper, or Scissors');
     }
 
-    return finalString;
+    return tally;
+}
+
+let game = () => {
+    playerWins = 0
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt('Choose Rock, Paper, or Scissors');
+        playerWins = playRound(playerChoice, getComputerChoice());
+    }
+
+    if (5 - playerWins <= 2) {
+        console.log("You won! You beat the computer!");
+    } else {
+        console.log("Sorry! You lost to the computer :(");
+    }
 }
 
 console.log(getComputerChoice())
 const playerSelection = 'rock';
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+game();
